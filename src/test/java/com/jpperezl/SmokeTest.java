d1e7e2677e45a909a7e662b88ed91806d3dfac03
jpperezl-work <jpperezl@outlook.com>
2025-08-24 03:14:23 -0500

    Commit: pom.xml complete configuration and Demo Test executed

---
+++ src/test/java/com/jpperezl/SmokeTest.java
@@ -0,0 +1,10 @@
+package com.jpperezl;
+
+import org.testng.annotations.Test;
+
+public class SmokeTest {
+    @Test
+    public void testDemo() {
+        System.out.println("✅ Setup funcionando con Selenium + RestAssured + TestNG + Allure");
+    }
+}
